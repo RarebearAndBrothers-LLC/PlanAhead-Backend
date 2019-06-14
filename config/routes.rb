c40268b83f826c29ab0f5c4bad29391e8497aad0
Ryan Ghaida <ryaboug@gmail.com>
2019-06-14 10:00:08 -0400

    user and habit models, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'habits/index'
+  get 'habits/new'
+  get 'habits/show'
+  get 'habits/edit'
+  get 'habits/destroy'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
