078f6e4b4ad8d1c32b41037409fd945193e76a3e
Ryan Ghaida <ryaboug@gmail.com>
2019-06-14 11:36:20 -0400

    username, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+ has_many :goals 
+ has_many :user_habits 
+ has_many :habits, through: :user_habits
 end
--- app/models/user_habit.rb
+++ app/models/user_habit.rb
@@ -1,2 +1,4 @@
 class UserHabit < ApplicationRecord
+    belongs_to :user 
+    belogns_to :habit
 end
