fc55d6a397a210cf204380302e7734efa0e3dea5
Ryan Ghaida <ryaboug@gmail.com>
2019-06-17 13:53:34 -0400

    JWT authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
+    post '/login', to: 'auth#create'
+    get '/profile', to: 'users#profile'
   resources :habits
   resources :goals
   resources :user_habits
